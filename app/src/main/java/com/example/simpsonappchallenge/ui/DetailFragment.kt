3a4a4cb6635dadffe1400876031ab352cba9e3cf
devafceballos <106552248+devafceballos@users.noreply.github.com>
2022-07-03 00:09:40 -0300

    added detailFragment.xml

---
+++ app/src/main/java/com/example/simpsonappchallenge/ui/DetailFragment.kt
@@ -0,0 +1,40 @@
+package com.example.simpsonappchallenge.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.simpsonappchallenge.R
+
+class DetailFragment : Fragment() {
+
+    private var param1: String? = null
+    private var param2: String? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_detail, container, false)
+    }
+
+    companion object {
+
+        @JvmStatic
+        fun newInstance(param1: String, param2: String) =
+            DetailFragment().apply {
+                arguments = Bundle().apply {
+
+                }
+            }
+    }
+}
